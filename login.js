c68270961916a41e2cb45274562e59d9e0c5bac7
KionkVan <kionvanemmanuel@gmail.com>
2025-04-13 09:04:25 +0100

    Added login.js and updated login redirect to index.html

---
+++ login.js
@@ -0,0 +1,19 @@
+import { auth } from "./firebase-config.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "welcome.html";
+    // Redirect to homepage on successful login
+  } catch (error) {
+    console.error("Error logging in: ", error);
+  }
+});
